477c2b5f69ea1d4befc9186a61ac08f5b29f148f
root <root@promote.cache-dns.local>
2014-11-18 17:38:34 +0800

    Storage.class.php commit

---
+++ ThinkPHP/Library/Think/Storage.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace Think;
+//分布式文件存储类
+class Storage{
+ 
+  static protected $handler;
+
+  static public function connect($type='File',$options=array()){
+     $class = 'Think\\Storage\\Driver\\'.ucwords($type);
+     self::$handler = new $class($options);
+  }
+
+  static public function __callstatic($method,$args){
+      if(method_exists(self::$handler,$method)){
+          return call_user_func_array(array(self::$handler,$method),$args);
+      }
+   }
+}
