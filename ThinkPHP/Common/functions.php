9b9700b546c64fcf28505be527792baa6af66f2e
root <root@promote.cache-dns.local>
2014-11-18 17:04:04 +0800

    functions.php commit 3

--- ThinkPHP/Common/functions.php
+++ ThinkPHP/Common/functions.php
@@ -1335,4 +1335,78 @@ function get_client_ip($type=0,$adv=false){
      $ip   = $long?array($ip,$long):array('0.0.0.0',0);
      return $ip[$type];
 }
- 
+
+
+/**
+ *发送HTTP状态
+ *@param integer $code状态码
+ *@return void
+ */
+function send_http_status($code){
+    static $_status = array(
+     //informational 1xx
+     100 => 'Continue',
+     101 => 'Switching Protocols',
+     //Success 2xx
+     200 => 'OK',
+     201 => 'Created',
+     202 => 'Accepted',
+     203 => 'Non-Authoritative information',
+     204 => 'No Content',
+     205 => 'Reset Content',
+     206 => 'Partial Content',
+     //Redirection 3xx
+     300 => 'Multiple Choices',
+     301 => 'Moved Permanently',
+     302 => 'Moved Temporarily', //1.1
+     303 => 'See Other',
+     304 => 'Not Modified',
+     305 => 'Use Proxy',
+     //306 is deprecated but reserved
+     307 => 'Temporary Redirect',
+     //Client Error 4xx
+     400 => 'Bad Request',
+     401 => 'Unauthorized',
+     402 => 'Payment Required',
+     403 => 'Forbidden',
+     404 => 'Not Found',
+     405 => 'Method Not Allowed',
+     406 => 'Not Acceptable',
+     407 => 'Proxy Authentication Required',
+     408 => 'Request Timeout',
+     409 => 'Conflict',
+     410 => 'Gone',
+     411 => 'Length Required',
+     412 => 'Precondition Failed',
+     413 => 'Request Entity Too Large',
+     414 => 'Request-URI Too Long',
+     415 => 'Unsupported Media Type',
+     416 => 'Requested Range Not Satisfiable',
+     417 => 'Expectation Failed',
+     // Server Error 5xx
+     500 => 'Internal Server Error',
+     501 => 'Not Implemented',
+     502 => 'Bad Gateway',
+     503 => 'Service Unavailable',
+     504 => 'Gateway Timeout',
+     505 => 'HTTP Version Not Supported',
+     506 => 'Sandwidth Limit Exceeded'
+   );
+   if(isset($_status[$code])){
+       header('HTTP/1.1 '.$code.' '.$_status[$code]);
+       //确保FastCGI模式下正常
+       header('Status:'.$code.' '.$_status[$code]);
+    }
+}
+
+//过滤表单中对表达式
+function filter_exp(&$value){
+   if(in_array(strtolower($value),array('exp','or'))){
+        $value .= ' ';
+   }
+}
+
+//不区分大小写对in_array实现
+function in_array_case($value,$array){
+   return in_array(strtolower($value),array_map('strtolower',$array));
+} 
